2acca383bf173d197c68c0badd10f2085be15522
divya gupta <shanu.divya123@gmail.com>
2017-07-16 00:26:26 +0530

    add image slider with audio

--- main.js
+++ main.js
@@ -1,8 +1,198 @@
 	
-			 var currentSongNumber = 1;
+				 var currentSongNumber = 1;
 			var willLoop = 0;
 			var willShuffle = 0; 
+	
+			
 		
+		
+	
+	
+		
+	function slide(next){
+		
+		if(next==songs[0]){
+	$('body').vegas('destroy');
+
+	$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/10.jpg" },
+        { src: "images/11.jpg" },
+        { src: "images/12.jpg" },
+        { src: "images/13.png" },
+        { src: "images/15.jpg" }
+    ],
+	animation: 'kenburns'
+		
+		
+	});
+	
+	}
+	else if(next ==songs[1]){
+	 $('body').vegas('destroy');
+		 
+	
+		
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/21.png" },
+        { src: "images/22.jpg" },
+        { src: "images/23.png" },
+        { src: "images/24.jpg" },
+        { src: "images/25.jpg" }
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	}
+	
+
+
+else if(next == songs[2]){
+	
+	 $('body').vegas('destroy');
+		
+		
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/30.jpg" },
+        { src: "images/31.jpg" },
+        { src: "images/32.jpg" },
+        { src: "images/33.png" },
+        { src: "images/34.jpg" },
+		 { src: "images/35.png" },
+		  { src: "images/36.jpg" }
+		  
+		
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	
+	}	
+	else if(next == songs[3]){
+		$('body').vegas('destroy');
+		
+		
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/41.jpg" },
+        { src: "images/42.jpg" },
+        { src: "images/43.jpg" },
+        { src: "images/45.jpg" },
+        { src: "images/46.jpg" },
+		 { src: "images/47.jpg" },
+		  { src: "images/48.jpg" },
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	
+	}
+	else if(next == songs[4]){
+	$('body').vegas('destroy');
+		
+		
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/51.jpg" },
+        { src: "images/52.jpg" },
+        { src: "images/53.jpg" },
+        { src: "images/54.jpg" },
+        { src: "images/55.jpg" },
+		{ src: "images/56.jpg" },
+	    { src: "images/57.jpg" },
+	    { src: "images/58.jpg" },
+		{ src: "images/59.jpg" },
+	    { src: "images/591.jpg" },
+	    { src: "images/592.jpg" },
+		{ src: "images/593.jpg" },
+		{ src: "images/595.jpg" },
+	    { src: "images/596.jpg" },
+		{ src: "images/597.jpg" },
+		{ src: "images/598.jpg" },
+		{ src: "images/599.jpg" }
+		
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	}
+	 else if(next == songs[5])
+	 {
+	 $('body').vegas('destroy');
+		
+		
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/60.jpg" },
+        { src: "images/61.jpg" },
+        { src: "images/62.jpg" },
+        { src: "images/63.jpg" },
+        { src: "images/64.jpg" },
+		{ src: "images/65.jpg" },
+	    { src: "images/66.jpg" },
+		 { src: "images/67.jpg" },
+	    
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	 }
+	else
+	{
+		$('body').vegas('destroy');
+		$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/01.jpg" },
+        { src: "images/02.jpg" },
+        { src: "images/04.jpg" },
+        { src: "images/05.jpg" },
+        { src: "images/06.jpg" }
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+	}
+	
+	}
+	
+	 
+	 
+
+	
+	
 	
 					
 				function fancyTimeFormat(time)
@@ -39,11 +229,13 @@
 			console.log('Playing');
 			$('.play-icon').removeClass('fa-play').addClass('fa-pause');
 			song.play();
+			$('body').vegas('play');
 			}
 			else {
 			console.log('Pausing'); //otherwise pause the song
 			$('.play-icon').removeClass('fa-pause').addClass('fa-play');
 			song.pause();
+			$('body').vegas('pause');
 			}
 			} 
 			
@@ -62,7 +254,14 @@
 		}
 		
 		
-		
+		function updateTimer() {
+			var song = document.querySelector('audio');
+			var ct = song.currentTime;
+			var td = song.duration;
+			var percentage =(ct/td)*100;
+			$('.progress-filled').css('width', percentage+ "%");
+			
+		}
 		
 		
 		
@@ -76,10 +275,13 @@
 				if(currentSong.search(songName) != -1)
 				{
 				toggleSong();
+					
+                 
 				}
 				else {
 				audio.src = songName;
 				toggleSong();
+				
 				changeCurrentSongDetails(songObj); 
 				}
 				});
@@ -99,20 +301,22 @@
 				'image': 'song2.jpeg'
 			},
 			{
-				'name': 'Ye Hai Aashiqui',
-				'artist': ' Abhishek Arora',
-				'album': 'Ye Hai Aashiqui',
-				'duration': '3:33',
-				'fileName': 'song3.mp3',
-				'image': 'song3.jpg'
+				'name': 'Khaab',
+				'artist': 'Akhil',
+				'album': 'Khaab',
+				'duration': '3:22',
+			   'fileName': 'song7.mp3',
+			   'image': 'song7.jpg'
+				
 			},
 			{
-				'name': 'Waka Waka',
-				'artist': 'Shakira',
-				'album': 'Sale el Sol',
-				'duration': '3:05',
-				'fileName': 'song4.mp3',
-				'image': 'song4.jpg'
+				'name': 'Na Ja Na Ja',
+				'artist': 'Pav Dharia',
+				'album': 'Na Ja Na Ja ',
+				'duration': '3:23',
+			   'fileName': 'song6.mp3',
+			   'image': 'song6.jpg'
+				
 			},
 			{
 			    'name': 'Shape Of You',
@@ -131,54 +335,72 @@
 				'image': 'song1.jpg'
 			},
 		    {
-				'name': 'Mahi Ve',
-				'artist': 'Neha Kakkar',
-				'album': 'Wajah Tum Ho',
-				'duration': '6:02',
-			   'fileName': 'song6.mp3',
-			   'image': 'song6.jpg'
-			},
-			{
-				'name': 'Khaab',
-				'artist': 'Akhil',
-				'album': 'Khaab',
-				'duration': '3:22',
-			   'fileName': 'song7.mp3',
-			   'image': 'song7.jpg'
-					
+				'name': 'Waka Waka',
+				'artist': 'Shakira',
+				'album': 'Sale el Sol',
+				'duration': '3:05',
+				'fileName': 'song4.mp3',
+				'image': 'song4.jpg'
+				
 			}]
-		  
-		  
+		 
+		   function timeJump(){
+		var song =document.querySelector('audio')
+		song.currentTime = song.duration-5;
+	}
+	 
 		  
 		  $('audio').on('ended',function() {
-				var nextSongNumber = randomExcluded(1,4,currentSongNumber);
+			   
+				var audio = document.querySelector('audio');
 					if (willShuffle == 1) {
-						var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
+						
+						var nextSongNumber = randomExcluded(1,6,currentSongNumber); // Calling our function from Stackoverflow
 						var nextSongObj = songs[nextSongNumber-1];
+						 
 						audio.src = nextSongObj.fileName;
 						toggleSong();
 						changeCurrentSongDetails(nextSongObj);
+					     slide(nextSongObj);
 						currentSongNumber = nextSongNumber;
+						
 					}
-					else if(currentSongNumber < 4) {
+					else if(currentSongNumber < 6) {
+						
 						var nextSongObj = songs[currentSongNumber];
 						audio.src = nextSongObj.fileName;
 						toggleSong();
 						changeCurrentSongDetails(nextSongObj);
+						slide(nextSongObj);
 						currentSongNumber = currentSongNumber + 1;
 					}
 					else if(willLoop == 1) {
+					
+						
 						var nextSongObj = songs[0];
 						audio.src = nextSongObj.fileName;
 						toggleSong();
 						changeCurrentSongDetails(nextSongObj);
 						currentSongNumber =  1;
+						slide(nextSongObj);
 					}
 					else {
+						slide(nextSongObj);
 						$('.play-icon').removeClass('fa-pause').addClass('fa-play');
 						audio.currentTime = 0;
 					}
 				})
+				
+				function randomExcluded(min,max,excluded){
+				var n =Math.floor(Math.random()*(max-min)+min);
+                if	(n>=excluded)n++;
+                return n;				
+					
+				}
+				
+				
+
+				
 				
 				
 							
@@ -189,6 +411,7 @@
 			updateCurrentTime(); 
 			setInterval(function() {
 			updateCurrentTime();
+			updateTimer();
 			},1000);
 		
 		
@@ -212,6 +435,8 @@
         song.find('.song-album').text(obj.album);
         song.find('.song-length').text(obj.duration);
         addSongNameClickEvent(obj,i+1);
+	
+		
 		}
 				
 			//addSongNameClickEvent(fileNames[0],1);
@@ -228,8 +453,7 @@
 		
 				
 				$('#songs').DataTable({
-				 "scrollY": "200px",
-					"scrollCollapse": true,
+				
 					"paging": false
 			});
 			}	   
@@ -239,11 +463,29 @@
 			//jquery ko bola class ko select kro or jb uspr click ho to code run kre
 			$('.welcome-screen button').on('click', function() { //$ is used  in place of var button = document.querySelecttor and 
 				var name = $('#name-input').val();               // name-input ki jo id hai uski jo value hai usko print krao
-				if (name.length > 2) {                          //name ki jo length hai agr greater than 2 hai to code run krega
+				if (name.length > 3) {                          //name ki jo length hai agr greater than 2 hai to code run krega
 					var message = "Welcome, " + name;
 					$('.main .user-name').text(message);        //jquery ko bolo main class mai h1 ko dhunde or usme text add kr de jo msg box mai hai
 					$('.welcome-screen').addClass('hidden');    //jquery ko bola wecome-screen class mai hidden class ko add kr do
-					$('.main').removeClass('hidden');          //then remove hidden class in main class
+					$('.main').removeClass('hidden');  
+					
+					$('.vegas-slider').vegas({
+		
+   slides:[
+        { src: "images/01.jpg" },
+        { src: "images/02.jpg" },
+        { src: "images/04.jpg" },
+        { src: "images/05.jpg" },
+        { src: "images/06.jpg" }
+    ],
+	animation: 'kenburns'
+		
+		
+		
+		
+		
+	});
+					//then remove hidden class in main class
 				} else {
 					$('#name-input').addClass('error');
 				}
@@ -282,6 +524,60 @@
 			
 		
 	
-		
-		
+
+
+$('#song1').on('click',function(){
 	
+slide(songs[0]);
+
+});
+
+
+
+
+
+$('#song2').on('click',function(){
+	
+ slide(songs[1]);
+		 
+		
+});
+
+$('#song3').on('click',function(){
+ 
+ slide(songs[2]);
+});
+
+
+
+$('#song4').on('click',function(){
+	slide(songs[3]);
+  
+});
+
+
+
+$('#song5').on('click',function(){
+	slide(songs[4]);
+  
+});
+
+
+
+$('#song6').on('click',function(){
+slide(songs[5]);
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
